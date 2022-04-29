6a3ac9e5e53120f04b75b7253e74e2ec49b11e44
abhi8779 <shekabhi51513@gmail.com>
2022-04-29 15:57:57 +0530

    Adding Add new expense functionality

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,5 +1,37 @@
 import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
 
-const ExpensesList = () => {};
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">No Expense Found ⚠️</h2>;
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+
+  //   return props.items.length > 0 ? (
+  //     props.items.map((expense) => (
+  //       <ExpenseItem
+  //         key={expense.id}
+  //         title={expense.title}
+  //         amount={expense.amount}
+  //         date={expense.date}
+  //       />
+  //     ))
+  //   ) : (
+  //     <p>No Expense Found ⚠️</p>
+  //   );
+};
 
 export default ExpensesList;
--- src/components/Expenses/RenderExpenses.js
+++ src/components/Expenses/RenderExpenses.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
+
 import "./RenderExpenses.css";
 
 function RenderExpenses(props) {
@@ -16,23 +17,11 @@ function RenderExpenses(props) {
     (expense) => expense.date.getFullYear().toString() === year
   );
 
-  let expenseContent = <p>No Expense Found ⚠️</p>;
-
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter activeYear={year} onYearChange={onYearChangeHandler} />
-      {expenseContent}
+      {/* {expenseContent} */}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
